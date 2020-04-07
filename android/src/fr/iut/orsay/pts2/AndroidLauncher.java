988fffa1ade75ed47c8d8f7c75a769075a143edd
69VR69 <44296242+69VR69@users.noreply.github.com>
2020-04-07 15:45:20 +0200

    setup libGDX

---
+++ android/src/fr/iut/orsay/pts2/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fr.iut.orsay.pts2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.iut.orsay.pts2.GameLauncher;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameLauncher(), config);
+	}
+}
--- app/src/main/java/fr/iut/orsay/pts2/TEST.java
+++
@@ -1,8 +0,0 @@
-package fr.iut.orsay.pts2;
-
-public class TEST {
-    Antonin FOUQUES
-    Massar ABBAS
-    Serrano Mattéo
-    Wylhem  Dorville
-}
