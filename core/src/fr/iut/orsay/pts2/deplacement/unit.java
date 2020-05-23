a8d0c393cc635001ff3935c05bfbada27b6d3848
Wylhem <wylhemdorv@gmail.com>
2020-05-23 16:38:57 +0200

    Deplacement

---
+++ core/src/fr/iut/orsay/pts2/deplacement/unit.java
@@ -0,0 +1,5 @@
+package fr.iut.orsay.pts2.deplacement;
+
+public class unit {
+
+}
---
+++ core/src/fr/iut/orsay/pts2/interfaces/ressource/Cost.java
@@ -0,0 +1,10 @@
+package fr.iut.orsay.pts2.interfaces.ressource;
+
+import java.util.HashMap;
+
+public class Cost {
+    /*HashMap<Ressource, Integer> ressources = new HashMap<>();// <objet ressource, quantité>
+    int time = 1;*/
+
+
+}
